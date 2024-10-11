c7bfacca4a876cee6b5a599cd9bbc8ef84cbc3aa
dmcmahon3 <dmcmahon3@ivytech.edu>
2024-10-11 13:21:03 -0400

    Update and rename main to main.py
    Renamed menu options and variable names for clarity.

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import sys
 import inputs
 
-print("PAYROLL CALCULATOR\n(by Group 1)")
+print("WEEKLY PAYROLL CALCULATOR\n(by Group 1)")
 #This function presents a list of options for users.
-def choices():
+def display_menu():
     print("\nOPTIONS:")
-    print("(1) View Employee Records")
-    print("(2) Add New Employee")
-    print("(3) Edit Employee")
-    print("(4) Remove Employee")
+    print("(1) View Employee Roster")
+    print("(2) Add Employee Record")
+    print("(3) Edit Employee Record")
+    print("(4) Delete Employee Record")
     print("(5) Calculate Pay")
-    print("(6) End Program")
+    print("(6) Exit")
 #A loop that prompts the user to select an option, then calls its function. 
 while True:
-    choices()
-    choice = input("\nSelect an option from 1-6: ")
-    if choice == "1":
+    display_menu()
+    menu_choice = input("\nSelect an option from 1-6: ")
+    if menu_choice == "1":
         inputs.view_roster()
-    elif choice == "2":
+    elif menu_choice == "2":
         inputs.add_record()  
-    elif choice == "3":
+    elif menu_choice == "3":
         inputs.edit_record()
-    elif choice == "4":
+    elif menu_choice == "4":
         inputs.delete_record()
-    elif choice == "5":
+    elif menu_choice == "5":
         inputs.view_roster()
         with open("calculation.py") as calculations:
             exec(calculations.read())   
-    elif choice == "6":
+    elif menu_choice == "6":
         sys.exit()          
     else:
         print ("\nInvalid selection.")
+
